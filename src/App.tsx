d5d159055ba753de080598a567ef203cdfae5163
Chris.Vasquez <cvasqu09@gmail.com>
2020-09-24 19:31:58 -0400

    Added styled components and started adding theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
 
-function App() {
+const theme = {
+  mode: 'light',
+  color: 'blue',
+};
+
+const A = styled.a`
+  color: ${(props) => props.theme.color};
+`;
+
+function App(): React.ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <header className="App-header">
+          <A href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </A>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
